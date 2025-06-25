568dc6a887dada729819fb4632b87c4ed409567b
icxldd <1278245922@qq.com>
2025-06-25 13:51:06 +0800

    1

--- other_project/EncodeLib/WindowsDllManager.cs
+++ other_project/EncodeLib/WindowsDllManager.cs
@@ -300,49 +300,10 @@ namespace EncodeLib
                 disposed = true;
             }
         }
+        // 进度回调委托定义
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ProgressCallback([MarshalAs(UnmanagedType.LPStr)] string filePath, double progress);
     }
 
-    // 支持的结构体定义
-    public enum T_PDU_IT
-    {
-        PDU_IT_IO_UNUM32 = 0x1000,
-        PDU_IT_IO_PROG_VOLTAGE = 0x1001,
-        PDU_IT_IO_BYTEARRAY = 0x1002,
-        PDU_IT_IO_FILTER = 0x1003,
-        PDU_IT_IO_EVENT_QUEUE_PROPERTY = 0x1004,
-        PDU_IT_RSC_STATUS = 0x1100,
-        PDU_IT_PARAM = 0x1200,
-        PDU_IT_RESULT = 0x1300,
-        PDU_IT_STATUS = 0x1301,
-        PDU_IT_ERROR = 0x1302,
-        PDU_IT_INFO = 0x1303,
-        PDU_IT_RSC_ID = 0x1400,
-        PDU_IT_RSC_CONFLICT = 0x1500,
-        PDU_IT_MODULE_ID = 0x1600,
-        PDU_IT_UNIQUE_RESP_ID_TABLE = 0x1700,
-        PDU_IT_VEHICLE_ID = 0x1800,
-        PDU_IT_ETH_SWITCH_STATE = 0x1801
-    }
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PDU_RSC_STATUS_DATA
-    {
-        public uint hMod;
-        public uint ResourceId;
-        public uint ResourceStatus;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PDU_RSC_STATUS_ITEM
-    {
-        public T_PDU_IT ItemType;
-        [MarshalAs(UnmanagedType.LPStr)]
-        public string name;
-        public uint NumEntries;
-        public PDU_RSC_STATUS_DATA pResourceStatusData;
-    }
-
-    // 进度回调委托定义
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void ProgressCallback([MarshalAs(UnmanagedType.LPStr)] string filePath, double progress);
 } 
